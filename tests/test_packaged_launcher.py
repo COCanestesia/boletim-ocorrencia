9dde99a96093e46b63610d300fb0c8ca1b54b451
COCanestesia <estrategiaequalidadecoc@gmail.com>
2026-09-22 10:07:57 -0400

    test: require reliable headless packaged startup

--- tests/test_packaged_launcher.py
+++ tests/test_packaged_launcher.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from launcher import streamlit_args
+from launcher import LOCAL_URL, streamlit_args
 
 
 def test_packaged_launcher_disables_first_run_prompt_and_fixes_local_port():
@@ -9,5 +9,6 @@ def test_packaged_launcher_disables_first_run_prompt_and_fixes_local_port():
     assert "--server.address=127.0.0.1" in args
     assert "--server.port=8765" in args
     assert "--server.showEmailPrompt=false" in args
-    assert "--server.headless=false" in args
+    assert "--server.headless=true" in args
     assert "--browser.gatherUsageStats=false" in args
+    assert LOCAL_URL == "http://127.0.0.1:8765"
