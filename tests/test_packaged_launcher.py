667d5dffc152a46d2ee6ba786d5027ceab819738
COCanestesia <estrategiaequalidadecoc@gmail.com>
2026-09-22 10:17:39 -0400

    test: require Streamlit release mode in packaged launcher

--- tests/test_packaged_launcher.py
+++ tests/test_packaged_launcher.py
@@ -6,6 +6,7 @@ from launcher import LOCAL_URL, streamlit_args
 def test_packaged_launcher_disables_first_run_prompt_and_fixes_local_port():
     args = streamlit_args(Path("app.py"))
 
+    assert "--global.developmentMode=false" in args
     assert "--server.address=127.0.0.1" in args
     assert "--server.port=8765" in args
     assert "--server.showEmailPrompt=false" in args
